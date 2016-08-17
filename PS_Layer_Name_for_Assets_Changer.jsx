66f6502a2b2374cee2a33e83b9007f073b26e101
Jan Gähler <kontakt@jan-gaehler.de>
2016-08-17 21:35:22 +0200

    init

---
+++ PS_Layer_Name_for_Assets_Changer.jsx
@@ -0,0 +1,32 @@
+/**
+ *  Layer Name for Assets Changer
+ *
+ *  Copyright 2016 by Jan Gähler
+ *
+ *  Licensed under Do What The Fuck You Want To Public License (WTFPL)
+ *  http://www.wtfpl.net/about/ 
+ */
+
+var doc = app.activeDocument;  
+
+for (var i = 0; i < doc.layers.length; i++) 
+{
+ 
+          var currentLayerName = doc.layers[i].name;
+
+          //currentLayerName = currentLayerName.toLowerCase();
+
+          newLayerName = currentLayerName + '.png, ' +
+          				'xcode/' + currentLayerName + '@3x.png, ' +
+          				'66.67% xcode/' + currentLayerName + '@2x.png, ' +
+          				'33.33% xcode/' + currentLayerName + '.png ,' +
+          				'xxhdpi/' + currentLayerName + '.png ,' +
+						'66.67% xhdpi/' + currentLayerName + '.png ,' +
+						'50% hdpi/' + currentLayerName + '.png ,' +
+						'33.33% mdpi/' + currentLayerName + '.png ,' +
+						'25% ldpi/' + currentLayerName + '.png';
+
+ 		//alert(newLayerName);
+
+          doc.layers[i].name = newLayerName;
+}
